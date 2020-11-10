c2e4be01251f04ed72c676620a1c751cc68235ef
Petee <petee82x@gmail.com>
2020-11-10 11:26:26 +0100

    Add a setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="supermodel",
+    version="0.0.1",
+    author="Peter Dolák",
+    author_email="peter@dolak.sk",
+    description="Models for conversion and validation of rich data structures.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/petee-d/supermodel",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)

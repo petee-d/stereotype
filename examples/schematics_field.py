7ebffd2454eeebc591720042f8b2467fae6af646
Peter Dolak <peter.dolak@exponea.com>
2022-07-08 10:28:08 +0200

    Document Schematics compatibility and migration

---
+++ examples/schematics_field.py
@@ -0,0 +1,35 @@
+from __future__ import annotations
+
+# Keep this file synchronized with the `stereotype/compat/README.md` documentation!
+
+from typing import Optional, List
+
+from schematics.models import Model as SchematicsModel
+from schematics.types import StringType, IntType
+
+from stereotype import Model, ListField
+from stereotype.compat.schematics import SchematicsModelField
+
+
+class Leaf(SchematicsModel):
+    color: str = StringType(required=True, default="green")
+
+
+class Fruit(SchematicsModel):
+    worms: Optional[int] = IntType()
+
+
+class Branch(Model):
+    length: float
+    fruit: Optional[Fruit] = SchematicsModelField(hide_none=True, default=None)
+    leaves: List[Leaf] = ListField(SchematicsModelField(), max_length=50)
+
+
+branch = Branch({'length': 42, 'leaves': [{'color': 'yellow'}, {}]})
+assert branch.leaves[1].color == 'green'
+assert branch.fruit is None
+assert branch.serialize() == {'length': 42, 'leaves': [{'color': 'yellow'}, {'color': 'green'}]}
+
+branch.fruit = Fruit({'worms': 2})
+assert branch.serialize()['fruit'] == {'worms': 2}
+branch.validate()

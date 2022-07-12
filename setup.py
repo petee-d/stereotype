5d1e1974a8bf437133770293a1bf61f63a9f49ac
Peter Dolak <peter.dolak@exponea.com>
2022-07-12 11:58:47 +0200

    Bump version to v1.1.0 for Pypi release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="stereotype",
-    version="1.0.1",
+    version="1.1.0",
     author="Peter Dolák",
     author_email="peter@dolak.sk",
     description="Models for conversion and validation of rich data structures.",
